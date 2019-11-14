741518ea90a5efec57fe9b98888a58f53776dbeb
Rsja12 <asocarras10@gmail.com>
2019-11-14 15:16:59 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ActiveRecord::Base
 
   belongs_to :author
   validate :is_title_case 
+  before_validation :make_title_case
+
+  before_save :email_author_about_post
 
   private
 
@@ -14,4 +17,9 @@ class Post < ActiveRecord::Base
   def make_title_case
     self.title = self.title.titlecase
   end
+
+  def email_author_about_post
+    # Not implemented. Just an example of what to use before_save for.
+  end
+
 end
